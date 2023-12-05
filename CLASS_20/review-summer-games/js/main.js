b53948ce1d7245f7056af6929f6c18525853243c
Angel Vazquez <angelcodes95@gmail.com>
2023-12-05 12:29:21 -0800

    summer-games array exercise

--- CLASS_20/review-summer-games/js/main.js
+++ CLASS_20/review-summer-games/js/main.js
@@ -1,1 +1,8 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multiAllNumsArray(arr){
+    let product = 1;
+    arr.forEach(num => product *= num)
+    alert(product)
+}
+// 2240
+multiAllNumsArray([1,2,4,8,7,5])
