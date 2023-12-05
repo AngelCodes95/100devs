08368157ef34b0a735eea143c6505a5e6b91654a
Angel Vazquez <angelcodes95@gmail.com>
2023-12-05 15:51:41 -0800

    winter-games exercise to drill arrays

--- CLASS_20/review-winter-games/js/main.js
+++ CLASS_20/review-winter-games/js/main.js
@@ -1,1 +1,12 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function evenFromArray(arr){
+    let evens = [];
+    arr.forEach(num => {
+        if(num % 2 === 0){
+            evens.push(num)
+        }
+    })
+    console.table(evens)
+}
+
+evenFromArray([1,2,3,4,5,6])
