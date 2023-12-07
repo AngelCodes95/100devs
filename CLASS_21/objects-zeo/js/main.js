a022f05feca99196f1231848dc303ddd7a1a3818
Angel Vazquez <angelcodes95@gmail.com>
2023-12-06 19:27:19 -0800

    objects excersize finished, powerRangersZeo

--- CLASS_21/objects-zeo/js/main.js
+++ CLASS_21/objects-zeo/js/main.js
@@ -1,1 +1,20 @@
 //Create a stopwatch object that has four properties and three methods
+
+let watch = {};
+
+watch.brand = 'nike';
+watch.color = 'black';
+watch.shape = 'round';
+watch.size = 'large';
+
+watch.start = function(){
+    console.log('watch starting');
+}
+
+watch.stop = function(){
+    console.log('stoping');
+}
+
+watch.sayBrand = function(){
+    console.log(watch.brand)
+}
