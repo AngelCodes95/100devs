3c41f2453c4cadedfafb52352fe9c58cc486a4e9
Angel Vazquez <angelcodes95@gmail.com>
2023-12-07 17:59:10 -0800

    objects-lost object training

--- CLASS_22/objects-lost-galaxy/js/main.js
+++ CLASS_22/objects-lost-galaxy/js/main.js
@@ -1,1 +1,17 @@
 //Create a mouse object that has four properties and three methods
+ let mouse = {}
+// properties of the mouse
+ mouse.brand = 'Logitech';
+ mouse.color = 'black';
+ mouse.model = 'MX Ergo';
+ mouse.wireless = true; 
+// methods of the mouse 
+mouse.leftClick = function (){
+    console.log('left click')
+}
+mouse.rightClick = function (){
+    console.log('right click')
+}
+mouse.scroll = function(){
+    console.log('scroll scroll scroll')
+}
