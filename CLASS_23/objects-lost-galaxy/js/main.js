ffcc93e01c8860a2dd06c684bf3d9c867d8051c2
Angel Vazquez <angelcodes95@gmail.com>
2023-12-10 21:35:41 -0800

    lost galaxy objects exercise completed

--- CLASS_23/objects-lost-galaxy/js/main.js
+++ CLASS_23/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a dog object that has four properties and three methods
+let dog  = {}
+
+dog.color = 'golden';
+dog.breed = 'retriever';
+dog.hair = 'long';
+dog.goodBoy = true;
+
+dog.bark = function(){
+    console.log('dog barked');
+}
+
+dog.jump = function(){
+    console.log('dog jumped');
+}
+
+dog.fetch = function(){
+    console.log('dog fetched item');
+}
