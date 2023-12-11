5a78fb31087fe4bb2821cb16c8c4f8ffedeb9dd4
Angel Vazquez <angelcodes95@gmail.com>
2023-12-11 07:44:27 -0800

    tony hawk object exercise using constructors

--- CLASS_23/objects-tony-hawk-pro-skater/js/main.js
+++ CLASS_23/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,19 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function TonyHawkFighter(characterName, characterStance, characterSponsor, characterSpecialMove){
+    this.name = characterName
+    this.stance = characterStance
+    this.sponsor = characterSponsor
+    this.special =characterSpecialMove
+    // methods 
+    this.flip = function() {
+        console.log('Character Kickflip!')
+    }
+    this.taunt = function(){
+        console.log(`Dont make me use my ${this.special}`)
+    }
+    this.grab = function(){
+        console.log('Melon Grab!')
+    }
+}
+
+let kareemCampbell = new TonyHawkFighter('Kareem', 'goofy', 'element', 'Ghetto Bird')
