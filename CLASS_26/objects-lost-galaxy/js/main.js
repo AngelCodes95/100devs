3b46992b41bcca0302fc9d6e5b2136344fc1b0fb
Angel Vazquez <angelcodes95@gmail.com>
2023-12-14 20:50:46 -0800

    lost galaxy object training completed

--- CLASS_26/objects-lost-galaxy/js/main.js
+++ CLASS_26/objects-lost-galaxy/js/main.js
@@ -1,1 +1,18 @@
-//Create a dog object that has four properties and three methods
+//Create a Pizza object that has four properties and three methods
+let pizza = {}
+    pizza.cheese = 'Mozzarella';
+    pizza.shape = 'round';
+    pizza.toppings =['cheese', 'pepperoni', 'jalapenos'];
+
+    pizza.crunch = function(){
+        console.log('What a crunchy bite');
+    }
+    pizza.slice = function(s){
+        s = Number(s);
+        console.log(`You grabbed ${s} slices!`);
+    }
+    pizza.frisbee = function(){
+        console.log('YEEEET');
+    }
+
+// so ... to create a new pizza.... we have to edit the code above. Instead we can make a constructor. 
