4c024815d7bc552c37d54c1b49102bedb22edc06
Angel Vazquez <angelcodes95@gmail.com>
2023-12-15 18:17:29 -0800

    dog api training

--- CLASS_26/objects-constructor-fun/js/main.js
+++ CLASS_26/objects-constructor-fun/js/main.js
@@ -1,1 +1,18 @@
 //Create a constructor with 4 properties and 3 methods
+function PizzaMachine(size, toppings, crust, sauce) {
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust; 
+    this.sauce = sauce;
+
+    this.estimatedDeliveryTime = function(){
+        console.log('Calculating...');
+    }
+    this.burnmouth() = function(){
+        console.log('FUUUUUUUUU');
+    }
+    this.frisbee = function(){
+        console.log('Yeet');
+    }
+}
+let pizza = new PizzaMachine('large', ['spinach', 'garlic'], 'deep dish', 'white');
