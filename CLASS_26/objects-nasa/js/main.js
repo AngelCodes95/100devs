75dc06650d277f51930c915f51ffd4bb14661cbf
Angel Vazquez <angelcodes95@gmail.com>
2023-12-20 10:57:20 -0800

    still working on API photo of day. Upload of class 27 materials

--- CLASS_26/objects-nasa/js/main.js
+++ CLASS_26/objects-nasa/js/main.js
@@ -1,10 +1,17 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
-fetch('https://api.nasa.gov/planetary/apod?api_key=')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.date)
-        // document.querySelector('img').src = 
-    })
-    .catch(err => {
-        console.log(`Error ${err}`)
-    });
+document.querySelector('button').addEventListener('click', getPhotoOfDay)
+
+function getPhotoOfDay(){
+    const userDate = document.querySelector('input').value 
+    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`
+
+
+fetch(apiUrl)
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+})
+.catch(err => {
+    console.log(`Error ${err}`)
+});
+}
