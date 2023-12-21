9b57fa95062de17b1e54f5d9599278845ef8225b
Angel Vazquez <angelcodes95@gmail.com>
2023-12-20 21:31:06 -0800

    netflix exercise for Class training

--- CLASS_27/objects-netflix-tv-shows/js/main.js
+++ CLASS_27/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,20 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+class MakeShow{
+    constructor(showType, showName, showDuration, showChannel){
+        this.show = showType;
+        this.name = showName;
+        this.duration = showDuration;
+        this.channel = showChannel;
+    }
+    closedCaption(){
+        console.log("Closed Captions are on")
+    }
+    pauseShow(){
+        alert('Show Paused!')
+    }
+    resumeShow(){
+        alert('Show Resumed')
+    }
+}
 
-
+let nedsDeclassified = new MakeShow('Survival Guide', 'Neds Declassified', '30 min', 'Nickelodeon')
