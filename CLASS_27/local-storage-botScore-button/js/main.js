5e2e37af5212713541281b4a5c5bd8d92c8dc455
Angel Vazquez <angelcodes95@gmail.com>
2023-12-31 09:09:13 -0800

    botScore local storage exercise

--- CLASS_27/local-storage-botScore-button/js/main.js
+++ CLASS_27/local-storage-botScore-button/js/main.js
@@ -12,28 +12,3 @@ function addAnothaOne(){
     scoreValue += 1;
     localStorage.setItem('botScore', scoreValue);
 }
-
-
-/* commenting this function out until i need pieces of it for api work.
-function getPhotoOfDay(){
-    const userDate = document.querySelector('input').value 
-    const api_key = 'hLA5sRyvo1ZQO4hxqsjbwoiMh4xFSc28RV21ulUz'
-    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${userDate}`
-
-
-fetch(apiUrl)
-.then(res => res.json())
-.then(data => {
-    console.log(data)
-    if (data.media_type === 'image'){
-        document.querySelector('img').src = data.url
-    }else if (data.media_type === 'video'){
-        document.querySelector('img').src = data.hdurl
-    }
-    document.querySelector('h3').innerText = data.explanation
-})
-.catch(err => {
-    console.log(`Error ${err}`)
-});
-} */
-
