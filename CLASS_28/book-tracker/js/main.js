e9184dc6054b34d770aa5a0f0be54c7c30bd261e
Angel Vazquez <angelcodes95@gmail.com>
2024-01-26 21:03:13 -0800

    v1.0.1 able to enter an isbn and have it stored in local storage. bug fixed that would cause initial value to return null

--- CLASS_28/book-tracker/js/main.js
+++ CLASS_28/book-tracker/js/main.js
@@ -1,5 +1,6 @@
-//Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
+
+document.querySelector('h2').innerText = localStorage.getItem('books')
 
 function getFetch(){
   const isbn = document.querySelector('input').value
@@ -9,10 +10,16 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
         console.log(data.title)
-        localStorage.setItem('books', data.title)
-        document.querySelector('h2').innerText += localStorage.getItem('books')
+        if(!localStorage.getItem('books')){
+          localStorage.setItem('books', data.title)
+        }else{
+          let books = localStorage.getItem('books') + " ; " + data.title
+          localStorage.setItem('books', books)
+        }
+        
+        // localStorage.setItem('books', data.title)
+        document.querySelector('h2').innerText = localStorage.getItem('books')
       })
       .catch(err => {
           console.log(`error ${err}`)
