1931b5746bcdf2c29ff158dd2952fa5f6b407378
Angel Vazquez <angelcodes95@gmail.com>
2024-01-29 14:40:31 -0800

    espresso machine class exercise

--- CLASS_30/objects-espresso-machine/js/main.js
+++ CLASS_30/objects-espresso-machine/js/main.js
@@ -1,1 +1,18 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class makeEspresso{
+    constructor(make, model, color, price){
+        this.make = make
+        this.model = model
+        this.color = color
+        this.price = price
+    }
+    froth(){
+        console.log('Frothing...')
+    }
+    brew(){
+        console.log('warming up...')
+    }
+    ice(){
+        console.log('2 ice cubes added...')
+    }
+}
