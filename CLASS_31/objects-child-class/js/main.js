831801dc6d1b5c494bd0d9dc791340eaccd349e3
Angel Vazquez <angelcodes95@gmail.com>
2024-01-31 20:59:00 -0800

    class exercises utilizing inheritance

--- CLASS_31/objects-child-class/js/main.js
+++ CLASS_31/objects-child-class/js/main.js
@@ -1,2 +1,17 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`)
+    }
+}
+class slug extends Animal{
+    constructor(name, type, shell){
+        super(name)
+        this.type = type
+        this.shell = shell
+    } 
+}
