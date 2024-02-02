506bf495e6a034edb1e8c465200f83d95515d3d3
Angel Vazquez <angelcodes95@gmail.com>
2024-02-01 20:18:00 -0800

    tweaked slug class, added Seal class for inheritance training

--- CLASS_31/objects-child-class/js/main.js
+++ CLASS_31/objects-child-class/js/main.js
@@ -8,10 +8,20 @@ class Animal{
         console.log(`${this.name} makes a sound`)
     }
 }
-class slug extends Animal{
+class Slug extends Animal{
     constructor(name, type, shell){
         super(name)
         this.type = type
         this.shell = shell
     } 
 }
+
+class Seal extends Animal{
+    constructor(name, size){
+        super(name)
+        this.size = size
+    }
+    arfArf(){
+        console.log('arf arf')
+    }
+} 
