fd28eee3004437e176a6c4eeb41543bb0efe610d
Angel Vazquez <angelcodes95@gmail.com>
2024-04-10 14:05:36 -0700

    added ejs and changed file to serve up an ejs index file instead of html

--- CLASS_36/first-crud-app/server.js
+++ CLASS_36/first-crud-app/server.js
@@ -11,19 +11,9 @@ mongoClient.connect(connectionString)
         // within the starwars database gather the quotes collection
         const quotesCollection = db.collection('quotes')
         console.log('Connected to database')
-        // route to fetch and render quotes
-        app.get('/', (req, res) => {
-            quotesCollection
-                .find()
-                .toArray()
-                .then(results => {
-                    // render quotes using HTML
-                    res.send(results)
-                    // console log for debugging
-                    console.log(results)
-                })
-                .catch(error => console.error(error))
-        })
+        
+        app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded( {extended: true}));
 
         // define the route to add new quotes
         app.post('/quotes', (req, res) => {
@@ -35,17 +25,24 @@ mongoClient.connect(connectionString)
               })
               .catch(error => console.error(error))
           })
+
+            // route to fetch and render quotes
+        app.get('/', (req, res) => {
+            quotesCollection
+                .find()
+                .toArray()
+                .then(results => {
+                    // console log for debugging
+                    console.log(results)
+                    res.render('index.ejs', { quotes: results });
+                })
+                .catch(error => console.error(error))     
+        });
+
+        app.listen(3000, function(){
+            console.log('listening on 3000')
+        })
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
 
-app.use(bodyParser.urlencoded( {extended: true}));
-
-app.listen(3000, function(){
-    console.log('listening on 3000')
-})
-
-app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/index.html')
-})
-
-console.log('May Node be with you')
+console.log('May Node be with you');
