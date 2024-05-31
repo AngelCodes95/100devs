d20e876dede867367403236dbdef10a4655a143f
Angel Vazquez <angelcodes95@gmail.com>
2024-05-31 10:23:18 -0700

    consumable api complete, not yet hosted, cors added

--- CLASS_39/fast-and-furious-api/server.js
+++ CLASS_39/fast-and-furious-api/server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 1320
 const characters = require('./characters')
+const cors = require('cors')
 
 
+// use cors module 
+app.use(cors())
 
 // load main html page when visiting home page
 app.get('/', (request, response) =>{
@@ -18,12 +21,12 @@ app.get('/api/:characterName', (req, res) => {
         res.json(characters[charactersName])
         console.log(`successfully responded with "${charactersName}'s" API data.`)
     }else{
-        throw console.error('Error, Not Available');
+        throw console.error('Error, Name  Not Available');
     }
 })
 
 // listen for the server on port variable or default
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The Server is running on ${PORT}! You better go catch it!`)
 })
 
