f11660248b8b6f74f7d6e1eda7b34310a0555ebf
Angel Vazquez <angelcodes95@gmail.com>
2024-07-18 21:34:52 -0700

    successfully authenticates with google and redirects to dashboard. Mongoose bug resolved

--- CLASS_45/stayingInLJ/inventory/config/passport.js
+++ CLASS_45/stayingInLJ/inventory/config/passport.js
@@ -9,7 +9,26 @@ module.exports = function(passport) {
         callbackURL: 'http://localhost:3000/auth/google/callback'
     }, 
         async (accessToken, refreshToken, profile, done) => {
-            console.log(profile)
+            const newUser = {
+                googleId: profile.id,
+                displayName: profile.displayName,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                image: profile.photos[0].value,
+            }
+
+            try{
+                let user = await User.findOne({ googleId: profile.id})
+
+                if(user){
+                    done(null, user)
+                }else {
+                    user = await User.create(newUser)
+                    done(null, user)
+                }
+            } catch (err){
+                console.error(err)
+            }
         }
     )
 )
@@ -17,7 +36,19 @@ module.exports = function(passport) {
     passport.serializeUser((user, done) => {
         done(null, user.id)
     })
-
+    /* BUG FIX :) Model.findById() in Mongoose no longer accepts a callback.  
     passport.deserializeUser((id, done) => {
         User.findById(id, (err, user) => done(err, user))})
+
+    The above code no longer works and has been replaced by async await, seen below */
+
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
+            done(null, user);
+        } catch (err) {
+            console.error(err)
+            done(err, null);
+        }
+    })
 }
